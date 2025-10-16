592a5f56d282083d5b7691e412593a32671fcd26
S.Nagachaitanyababu <nagachaitanyababusiga@gmail.com>
2025-10-16 16:17:37 +0530

    adding few changes to modifiers

--- C/Modifiers.c
+++ C/Modifiers.c
@@ -1,1 +1,11 @@
 #include <stdio.h>
+
+int main(){
+    signed int a=234;
+    signed int b=-234;
+    unsigned int c=23;
+    unsigned int d=-234;
+    long e=2343434;
+    printf("a=%d, b=%d,c=%d,d=%d, e=%d\n",a,b,c,d,e);
+    printf("a=%d, b=%d,c=%d,d=%d, e=%d",sizeof(a),sizeof(b),sizeof(c),sizeof(d),sizeof(e));
+}
